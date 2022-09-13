2fcfc07a08d1699c2826747cfec45f9cb827e2f0
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 13:30:14 -0300

    test: implementing all validation test cases

--- src/modules/asset/use-cases/CreateAssetUseCase.spec.ts
+++ src/modules/asset/use-cases/CreateAssetUseCase.spec.ts
@@ -19,9 +19,29 @@ describe('CreateAssetUseCase', () => {
     }
   });
 
-  it('should break due to validation: description');
+  it('should break due to validation: description', async () => {
+    try {
+      await createAssetUseCase.execute({
+        name: 'My New Asset',
+        description: '',
+        imageUrl: 'https://picsum.photos/200/300',
+      });
+    } catch (error: any) {
+      expect(error.message).toBe('Description is too short!');
+    }
+  });
 
-  it('should break due to validation: image');
+  it('should break due to validation: image', async () => {
+    try {
+      await createAssetUseCase.execute({
+        name: 'My New Asset',
+        description: 'Description here',
+        imageUrl: '',
+      });
+    } catch (error: any) {
+      expect(error.message).toBe('Image URL is required');
+    }
+  });
 
   it('should successfully create an asset');
 });

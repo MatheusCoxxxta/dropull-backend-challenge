4a265c1dbd54892882cf247d8c3870d108152ad0
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 17:14:20 -0300

    feat: using in-memory database for a while

--- src/modules/asset/infra/typeorm/repositories/AssetsRepository.ts
+++ src/modules/asset/infra/typeorm/repositories/AssetsRepository.ts
@@ -3,12 +3,27 @@ import IAsset from '@modules/asset/entities/IAsset';
 import IAssetsRepository from '@modules/asset/use-cases/ports/IAssetsRepository';
 
 export default class AssetsRepository implements IAssetsRepository {
+  private asset: IAsset[] = [];
   save({
     name,
     description,
     imageUrl,
     tokenIpfs,
   }: IStoreAssetDto): Promise<IAsset> {
-    throw new Error('Method not implemented.');
+    const asset = {
+      id: 'randon-id',
+      name,
+      description,
+      imageUrl,
+      tokenIpfs,
+    };
+
+    this.asset.push(asset);
+
+    return Promise.resolve(asset);
+  }
+
+  listAll(): Promise<IAsset[]> {
+    return Promise.resolve(this.asset);
   }
 }

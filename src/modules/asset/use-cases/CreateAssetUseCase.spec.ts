d053e23bec52785d7ff3c9fb3ef080e955d78d68
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 12:20:32 -0300

    feat: basic test suite

---
+++ src/modules/asset/use-cases/CreateAssetUseCase.spec.ts
@@ -0,0 +1,9 @@
+import CreateAssetUseCase from './CreateAssetUseCase';
+
+let createAssetUseCase: CreateAssetUseCase;
+
+describe('CreateAssetUseCase', () => {
+  beforeEach(() => {
+    createAssetUseCase = new CreateAssetUseCase();
+  });
+});

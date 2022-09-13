31a98b3b1879eccb60436e0c8df0d640bb2668ee
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 18:45:55 -0300

    feat: creating database connection

---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+import Assets from '../../../modules/asset/infra/typeorm/entities/Assets';
+
+export const AppDataSource = new DataSource({
+  synchronize: true, // Note: using sync only to make easy to run app on guest PCs.
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: false,
+  entities: [Assets],
+  subscribers: [],
+  migrations: [],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database connected!');
+  })
+  .catch((error) => console.log(error));

bf886ae8a6466718599aef3238db6debb8ad41e2
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 12:21:06 -0300

    feat: implementing basic use-case

---
+++ src/modules/asset/use-cases/CreateAssetUseCase.ts
@@ -0,0 +1,7 @@
+import IUseCase from './IUseCase';
+
+export default class CreateAssetUseCase implements IUseCase {
+  async execute(data: any): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}

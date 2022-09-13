061e1fe6873273555081bca78c4b4aa220ab6503
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 15:22:08 -0300

    feat: defining post endpoint to create asset

--- src/modules/asset/infra/http/routes/asset.routes.ts
+++ src/modules/asset/infra/http/routes/asset.routes.ts
@@ -1,5 +1,20 @@
 import { Router } from 'express';
+import CreateAssetController from '../controllers/CreateAssetController';
+import { Joi, Segments, celebrate } from 'celebrate';
+
+const createAssetController = new CreateAssetController();
 
 const assetsRouter = Router();
+
+assetsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+    },
+  }),
+  createAssetController.handle,
+);
 
 export default assetsRouter;

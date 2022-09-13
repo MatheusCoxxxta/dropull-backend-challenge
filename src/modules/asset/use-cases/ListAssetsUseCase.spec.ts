7312e5e7fb1c3c086c30fb4ddd7204495949adc9
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 17:06:45 -0300

    test:  adding basic list test case

--- src/modules/asset/use-cases/ListAssetsUseCase.spec.ts
+++ src/modules/asset/use-cases/ListAssetsUseCase.spec.ts
@@ -1,6 +1,7 @@
 import ListAssetsUseCase from './ListAssetsUseCase';
 import IAssetsRepository from './ports/IAssetsRepository';
 import FakeAssetRepository from '../repositories/fakes/FakeAssetsRepository';
+import IAsset from '../entities/IAsset';
 
 let listAssetsUseCase: ListAssetsUseCase;
 let assetsRepository: IAssetsRepository;
@@ -9,5 +10,11 @@ describe('ListAssetsUseCase', () => {
   beforeEach(() => {
     assetsRepository = new FakeAssetRepository();
     listAssetsUseCase = new ListAssetsUseCase(assetsRepository);
+  });
+
+  it('should list all registered assets', async () => {
+    const assets = await listAssetsUseCase.execute();
+
+    expect(Array.isArray(assets)).toBe(true);
   });
 });

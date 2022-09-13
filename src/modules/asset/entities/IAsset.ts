27ad032ca1febc7db51e9b7ee917b28fff351dd4
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 19:35:38 -0300

    feat: adding attr nfts to asset

--- src/modules/asset/entities/IAsset.ts
+++ src/modules/asset/entities/IAsset.ts
@@ -1,7 +1,11 @@
+import INft from './INft';
+
 export default interface IAsset {
   id: string;
   name: string;
   description: string;
   imageUrl: string;
   tokenIpfs: string;
+
+  nfts?: Array<INft>;
 }

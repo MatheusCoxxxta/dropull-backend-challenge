b32aaa66245de4a4b9c647ecbaa76db0f57f9de6
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 12:02:21 -0300

    feat: adding error middleware to handle exceptions

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,25 @@
-import express from "express";
+import AppError from '@shared/errors/AppError';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
+
+app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: err.statusCode,
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333);

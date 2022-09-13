cee56d39ac14551bb57eafbdfc149b4de74d1b26
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 19:59:45 -0300

    feat: implementing typeorm repository

---
+++ src/modules/asset/infra/typeorm/repositories/NftRepository.ts
@@ -0,0 +1,17 @@
+import INft from '@modules/asset/entities/INft';
+import INftRepository from '@modules/asset/use-cases/ports/INftRepository';
+import { AppDataSource } from '@shared/infra/typeorm';
+import { Repository } from 'typeorm';
+import Nft from '../entities/Nft';
+
+export default class NftRepository implements INftRepository {
+  private repository: Repository<Nft>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(Nft);
+  }
+
+  async listAll(): Promise<Array<INft>> {
+    return this.repository.find();
+  }
+}

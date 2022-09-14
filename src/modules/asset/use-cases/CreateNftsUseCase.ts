28a4fe9f8e8e8944ce6938a8ade1e9057309a151
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 21:56:26 -0300

    feat: implementing use case

--- src/modules/asset/use-cases/CreateNftsUseCase.ts
+++ src/modules/asset/use-cases/CreateNftsUseCase.ts
@@ -1,8 +1,23 @@
 import ICreateNftsDto from '../dtos/ICreateNftsDto';
+import INft from '../entities/INft';
+import Nft from '../entities/Nft';
 import INftRepository from './ports/INftRepository';
+import IUseCase from './ports/IUseCase';
 
-export default class CreateNftsUseCase {
+export default class CreateNftsUseCase implements IUseCase {
   constructor(private nftRepository: INftRepository) {}
 
-  async execute({ amount, assetId }: ICreateNftsDto) {}
+  async execute({ amount, assetId }: ICreateNftsDto): Promise<Array<INft>> {
+    const validPayload = Nft.validate(amount, assetId);
+
+    let nftList: Array<INft> = [];
+
+    for (let index = 1; index <= validPayload.amount; index++) {
+      const storedNft = await this.nftRepository.save(validPayload.assetId);
+
+      nftList = [...nftList, storedNft];
+    }
+
+    return nftList;
+  }
 }

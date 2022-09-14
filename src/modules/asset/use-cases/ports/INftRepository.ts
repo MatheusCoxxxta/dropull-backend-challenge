f05251def8cf2310575a390f59cfe2e64ed2131b
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 22:44:54 -0300

    feat: defining new method create

--- src/modules/asset/use-cases/ports/INftRepository.ts
+++ src/modules/asset/use-cases/ports/INftRepository.ts
@@ -1,6 +1,8 @@
+import IStoreNftDto from '@modules/asset/dtos/IStoreNftDto';
 import INft from '@modules/asset/entities/INft';
 
 export default interface INftRepository {
   listAll(): Promise<Array<INft>>;
-  save(assetId: string): Promise<INft>;
+  create(assetId: string): Promise<INft>;
+  save({ assetId, token }: IStoreNftDto): Promise<INft>;
 }

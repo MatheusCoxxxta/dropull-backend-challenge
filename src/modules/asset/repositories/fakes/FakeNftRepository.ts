398d6bb57103cb0eca16d288c01d178e2514e4aa
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 22:45:24 -0300

    test: implemeting fake method create

--- src/modules/asset/repositories/fakes/FakeNftRepository.ts
+++ src/modules/asset/repositories/fakes/FakeNftRepository.ts
@@ -1,4 +1,5 @@
 import IStoreAssetDto from '@modules/asset/dtos/IStoreAssetDto';
+import IStoreNftDto from '@modules/asset/dtos/IStoreNftDto';
 import IAsset from '@modules/asset/entities/IAsset';
 import INft from '@modules/asset/entities/INft';
 import IAssetsRepository from '@modules/asset/use-cases/ports/IAssetsRepository';
@@ -17,10 +18,26 @@ export default class FakeNftRepository implements INftRepository {
     return Promise.resolve(this.nft);
   }
 
-  save(assetId: string): Promise<INft> {
+  create(assetId: string): Promise<INft> {
     const nft = {
       id: 'my-id',
       token: 'my-token',
+      asset: {
+        id: assetId,
+        name: 'My Asset',
+        description: 'My Description',
+        imageUrl: 'https://picsum.photos/200/300',
+        tokenIpfs: 'my-tokenIpfs',
+      },
+    };
+
+    return Promise.resolve(nft);
+  }
+
+  save({ assetId, token }: IStoreNftDto): Promise<INft> {
+    const nft = {
+      id: 'my-id',
+      token,
       asset: {
         id: assetId,
         name: 'My Asset',

25a59fca5b55fcb585e3812c7004a855f45d1692
Matheus Costa <matheus271200.ms@gmail.com>
2022-09-13 22:51:43 -0300

    feat: check for asset and using Asset TokenIpfs to generate NFT token

--- src/modules/asset/use-cases/CreateNftsUseCase.ts
+++ src/modules/asset/use-cases/CreateNftsUseCase.ts
@@ -1,19 +1,33 @@
+import AppError from '@shared/errors/AppError';
 import ICreateNftsDto from '../dtos/ICreateNftsDto';
 import INft from '../entities/INft';
 import Nft from '../entities/Nft';
+import IAssetsRepository from './ports/IAssetsRepository';
 import INftRepository from './ports/INftRepository';
 import IUseCase from './ports/IUseCase';
 
 export default class CreateNftsUseCase implements IUseCase {
-  constructor(private nftRepository: INftRepository) {}
+  constructor(
+    private nftRepository: INftRepository,
+    private assetsRepository: IAssetsRepository,
+  ) {}
 
   async execute({ amount, assetId }: ICreateNftsDto): Promise<Array<INft>> {
     const validPayload = Nft.validate(amount, assetId);
 
+    const asset = await this.assetsRepository.findById(validPayload.assetId);
+
+    if (!asset) throw new AppError('Asset not found!');
+
     let nftList: Array<INft> = [];
 
     for (let index = 1; index <= validPayload.amount; index++) {
-      const storedNft = await this.nftRepository.save(validPayload.assetId);
+      const nft = await this.nftRepository.create(validPayload.assetId);
+
+      const storedNft = await this.nftRepository.save({
+        assetId: validPayload.assetId,
+        token: `${asset.tokenIpfs}-${nft.id}`,
+      });
 
       nftList = [...nftList, storedNft];
     }
